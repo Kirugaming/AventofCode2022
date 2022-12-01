125feba7ec3d06eb94b2216fdd9934bd13c44e25
Kiru <Kiruthegreat2@gmail.com>
2022-12-01 14:39:44 -0600

    Day 1 - Calorie Counting part 2

--- Solutions/CalorieCounting/CalorieCounting.cpp
+++ Solutions/CalorieCounting/CalorieCounting.cpp
@@ -14,21 +14,45 @@ int main() {
 
     std::string line;
     int totalCalories = 0;
-    int maxCalories = 0;
+    int top1Calories = 0;
+    int top2Calories = 0;
+    int top3Calories = 0;
+
+    // PART 1
 
     while(std::getline(input, line)) {
         if (!line.empty()) {
             int calories = std::stoi(line);
             totalCalories += calories;
         } else {
-            if (totalCalories > maxCalories) {
-                maxCalories = totalCalories;
-            }
+                // PART 2
+                if (totalCalories > top3Calories) {
+                    if (totalCalories > top2Calories) {
+                        if (totalCalories > top1Calories) {
+                            top3Calories = top2Calories;
+                            top2Calories = top1Calories;
+                            top1Calories = totalCalories;
+                        } else {
+                            top3Calories = top2Calories;
+                            top2Calories = totalCalories;
+                        }
+                    } else {
+                        top3Calories = totalCalories;
+                    }
+                }
+                //
+
+                //top1Calories = totalCalories;
             totalCalories = 0;
         }
     }
 
-    std::cout << maxCalories << std::endl;
+    std::cout << top1Calories << std::endl;
+    std::cout << top1Calories + top2Calories + top3Calories << std::endl;
+
+    // PART 2
+
+
 
 
 
